aa3939381d69bccfd7bddfafff284733d5db068f
guzmanespinoza <luisespinoza.le109@gmail.com>
2022-04-24 21:03:15 -0600

    Configuracion de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./quiniela/qiniela.module').then(m => m.QuinielaModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/quiniela/qiniela.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { QinielaRoutingModule } from './qiniela-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    QinielaRoutingModule
+  ]
+})
+export class QuinielaModule { }
